59c8d508edc8dabcb550fb40375af73359025384
Wendell Smith <wackywendell@gmail.com>
2018-07-25 10:34:52 -0400

    Started with clap

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,11 @@
 #![warn(rust_2018_idioms)]
 
 fn main() {
+    clap::App::new("rchat")
+        .version("0.1")
+        .about("An experimental chat server.")
+        .author("Wendell Smith")
+        .get_matches();
+
     println!("Hello, world!");
 }
