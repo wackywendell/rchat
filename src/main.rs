9cec9f0099e5d79fba53ac8f23a9d7a4ce35af9c
Wendell Smith <wackywendell@gmail.com>
2018-07-25 11:09:23 -0400

    Started using clap subcommand

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,17 @@
 #![warn(rust_2018_idioms)]
 
 fn main() {
+    let server_subparser = clap::SubCommand::with_name("serve").arg(
+        clap::Arg::with_name("port")
+            .short("p")
+            .help("Set port to serve on"),
+    );
+
     clap::App::new("rchat")
         .version("0.1")
         .about("An experimental chat server.")
         .author("Wendell Smith")
+        .subcommand(server_subparser)
         .get_matches();
 
     println!("Hello, world!");
