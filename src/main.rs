03c321f7a7bde9f850ddb2d29621ba63eeb7c9e8
Wendell Smith <wackywendell@gmail.com>
2018-07-26 10:19:24 -0500

    Linked in protos lib

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 #![feature(rust_2018_preview)]
 #![warn(rust_2018_idioms)]
 
+#[derive(Clone)]
+struct ChatServer {
+    members: Vec<String>,
+}
+
+impl protos::chat_grpc::Serve for ChatServer {
+    fn register(
+        &self,
+        _ctx: grpcio::RpcContext<'_>,
+        _req: protos::chat::Registration,
+        _sink: grpcio::UnarySink<protos::chat::Registered>,
+    ) {
+
+    }
+}
+
 fn main() {
     let server_subparser = clap::SubCommand::with_name("serve").arg(
         clap::Arg::with_name("port")
